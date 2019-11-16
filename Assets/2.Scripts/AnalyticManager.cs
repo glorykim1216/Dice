7e6c1a79aab30727ada63592e3ba2519be5deae4
glorykim <glorykim1216@gmail.com>
2019-11-16 20:45:39 +0900

    파이어베이스 Analytic

---
+++ Assets/2.Scripts/AnalyticManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnalyticManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Firebase.Analytics.FirebaseAnalytics.LogEvent("Login");
+        Firebase.Analytics.FirebaseAnalytics.LogEvent("Score", "percent", Random.Range(0, 100));
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}

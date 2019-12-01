b07f4e5dbbbae62a455d22567136a9442dc5948a
glorykim <glorykim1216@gmail.com>
2019-12-01 21:36:48 +0900

    크로스배너 적용

---
+++ Assets/2.Scripts/CrossBanner/CrossBanner.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CrossBanner : MonoBehaviour
+{
+    public Button crossBannerBtn;
+
+    public Image crossBannerImg;
+    public Sprite[] crossBannerSprites;
+
+    void Start()
+    {
+        crossBannerBtn.onClick.AddListener(OpenPlayStore);
+
+        crossBannerSprites = Resources.LoadAll<Sprite>("CrossBanner");
+
+        StartCoroutine(cor_CrossBannerAnim());
+    }
+
+    IEnumerator cor_CrossBannerAnim()
+    {
+        while (true)
+        {
+            for (int i = 0; i < crossBannerSprites.Length; i++)
+            {
+                yield return new WaitForSeconds(0.05f);
+                crossBannerImg.sprite = crossBannerSprites[i];
+            }
+        }
+    }
+
+    public void OpenPlayStore()
+    {
+        Application.OpenURL("market://details?id=com.planb.goatjump");
+    }
+
+}
